5878296e966b6ecaf8eb6f70030605c7c35c102e
softchris <noringc@gmail.com>
2018-10-27 21:10:38 +0100

    refactor: revert route change

--- src/heroes/Heroes.js
+++ src/heroes/Heroes.js
@@ -39,7 +39,7 @@ class Heroes extends Component {
   };
 
   putHeroesApi = async hero => {
-    const response = await axios.put(`${API}/heroes/${hero.id}`, hero);
+    const response = await axios.put(`${API}/hero/${hero.id}`, hero);
     if (response.status !== 200) throw Error(response.message);
     return response.data;
   };
