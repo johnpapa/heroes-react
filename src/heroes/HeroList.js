ca8da02174b4921ab79c0c7846641efcbd41a669
John Papa <john@johnpapa.net>
2018-10-29 12:25:32 -0400

    format

--- src/heroes/HeroList.js
+++ src/heroes/HeroList.js
@@ -23,54 +23,54 @@ class HeroList extends Component {
       <div className="panel">
         <h3 className="panel-heading">Hero List</h3>
         <div className="panel-block">
-      <ul className="list">
-        {heroes.map((hero, index) => (
-          <li key={hero.id} role="presentation">
-            <div className="columns is-variable is-2 is-single-line">
-              <div className="column is-narrow icons" />
-              <div className="column">
-                <a
-                  data-index={index}
-                  onClick={this.selectHero}
-                  aria-label="select"
-                  role="button"
-                  tabIndex={0}
-                  >
-                  <article
-                    className={
-                      'box content list-item ' +
-                      (selectedHero && selectedHero.id === hero.id
-                        ? 'selected'
-                        : '')
-                    }
-                  >
-                    <div className="name">{hero.name}</div>
-                    <div className="description">{hero.description}</div>
-                  </article>
-                </a>
-              </div>
-              <div className="column is-narrow icons">
-                <nav className="level is-mobile">
-                  <div className="level-right delete-item">
+          <ul className="list">
+            {heroes.map((hero, index) => (
+              <li key={hero.id} role="presentation">
+                <div className="columns is-variable is-2 is-single-line">
+                  <div className="column is-narrow icons" />
+                  <div className="column">
                     <a
-                      className="level-item button-icon"
                       data-index={index}
-                      data-hero-id={hero.id}
-                      onClick={this.deleteHero}
-                      aria-label="delete"
+                      onClick={this.selectHero}
+                      aria-label="select"
                       role="button"
                       tabIndex={0}
                     >
-                      <i className="fas fa-trash-alt" aria-hidden="true" />
+                      <article
+                        className={
+                          'box content list-item ' +
+                          (selectedHero && selectedHero.id === hero.id
+                            ? 'selected'
+                            : '')
+                        }
+                      >
+                        <div className="name">{hero.name}</div>
+                        <div className="description">{hero.description}</div>
+                      </article>
                     </a>
                   </div>
-                </nav>
-              </div>
-            </div>
-          </li>
-        ))}
-      </ul>
-      </div>
+                  <div className="column is-narrow icons">
+                    <nav className="level is-mobile">
+                      <div className="level-right delete-item">
+                        <a
+                          className="level-item button-icon"
+                          data-index={index}
+                          data-hero-id={hero.id}
+                          onClick={this.deleteHero}
+                          aria-label="delete"
+                          role="button"
+                          tabIndex={0}
+                        >
+                          <i className="fas fa-trash-alt" aria-hidden="true" />
+                        </a>
+                      </div>
+                    </nav>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     );
   }
