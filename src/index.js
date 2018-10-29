c6ce34a26d21d8a2b3e965731d9ebad2aaa4b9e8
John Papa <john@johnpapa.net>
2018-10-28 22:25:06 -0400

    added redux tools

--- src/index.js
+++ src/index.js
@@ -6,26 +6,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import app from './store';
 import { watchLoadingHeroesAsync } from './heroes/hero.saga';
 
 // create and configure reduxer middleware ( saga is a middleware )
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(
-  app,
-  applyMiddleware(sagaMiddleware)
-)
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(app, composeEnhancers(
+// const store = createStore(
+  // app,
+  applyMiddleware(sagaMiddleware),
+  // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+));
 
 // trigger saga to start
-sagaMiddleware.run(watchLoadingHeroesAsync)
+sagaMiddleware.run(watchLoadingHeroesAsync);
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-document.getElementById('root'));
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
