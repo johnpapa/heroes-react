954ea7828fdc511427947d291e6ba3fbd65b2107
John Papa <john@johnpapa.net>
2018-10-29 12:20:55 -0400

    refactored the NavMenu out

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import { Link, Router } from '@reach/router';
+import { Router } from '@reach/router';
 import 'bulma/css/bulma.css';
 import React, { Component } from 'react';
 import Header from './Header';
 import Heroes from './heroes/Heroes';
 import List from './containers/List';
+import Nav from './Nav';
 import './styles.scss';
-
-const NavLink = props => (
-  <Link
-    {...props}
-    getProps={({ isCurrent }) => {
-      return {
-        className: isCurrent ? 'activeLink' : ''
-      };
-    }}
-  />
-);
 
 class App extends Component {
   render() {
@@ -23,15 +13,7 @@ class App extends Component {
       <div>
         <Header />
         <div className="section columns">
-          <nav className="column is-2 menu">
-            <p className="menu-label">Menu</p>
-            <ul className="menu-list">
-              <NavLink to="heroes">Heroes</NavLink>
-              <NavLink to="villains">Villains</NavLink>
-              <NavLink to="about">About</NavLink>
-            </ul>
-          </nav>
-
+          <Nav />
           <main className="column">
             <Router>
               <Heroes path="/heroes" />
