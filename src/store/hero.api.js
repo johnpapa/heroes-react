6590045a5318b4ce984ba362e92d65dbc33c30ca
John Papa <john@johnpapa.net>
2018-11-11 13:53:30 +0100

    api guard logic

--- src/store/hero.api.js
+++ src/store/hero.api.js
@@ -1,28 +1,25 @@
 import axios from 'axios';
+import { parseItem, parseList } from './action-utils';
 import { API } from './config';
 const captains = console;
 
 export const deleteHeroApi = async hero => {
   const response = await axios.delete(`${API}/heroes/${hero.id}`);
-  if (response.status !== 200) throw Error(response.message);
-  return response.data;
+  return parseItem(response, 200);
 };
 
 export const updateHeroApi = async hero => {
   captains.log(hero.id);
   const response = await axios.put(`${API}/heroes/${hero.id}`, hero);
-  if (response.status !== 200) throw Error(response.message);
-  return response.data;
+  return parseItem(response, 200);
 };
 
 export const addHeroApi = async hero => {
   const response = await axios.post(`${API}/heroes`, hero);
-  if (response.status !== 201) throw Error(response.message);
-  return response.data;
+  return parseItem(response, 201);
 };
 
 export const loadHeroesApi = async () => {
   const response = await axios.get(`${API}/heroes`);
-  if (response.status !== 200) throw Error(response.message);
-  return response.data;
+  return parseList(response, 200);
 }
