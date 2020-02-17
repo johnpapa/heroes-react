d6e3794dce6544fa94c2bf73cc4177c00575e99f
Ward Bell <wardbell@hotmail.com>
2020-02-17 07:04:46 -0800

    Convert Hero component classes to functions with hooks

--- src/heroes/HeroList.js
+++ src/heroes/HeroList.js
@@ -1,58 +1,115 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { withRouter } from 'react-router';
 
 import { ButtonFooter, CardContent } from '../components';
 
-class HeroList extends Component {
-  selectHero = e => {
-    const index = +e.currentTarget.dataset.index;
-    const { heroes } = this.props;
-    const hero = heroes[index];
+function HeroList({ handleDeleteHero, handleSelectHero, heroes, history }) {
 
-    this.props.handleSelectHero(hero);
-    this.props.history.push(`/heroes/${hero.id}`);
-  };
-
-  deleteHero = e => {
-    const index = +e.currentTarget.dataset.index;
-    const { heroes } = this.props;
-    const hero = heroes[index];
-    this.props.handleDeleteHero(hero);
-  };
-
-  render() {
-    let { heroes /* , selectedHero */ } = this.props;
-
-    return (
-      <ul className="list">
-        {heroes.map((hero, index) => (
-          <li key={hero.id} role="presentation">
-            <div className="card">
-              <CardContent name={hero.name} description={hero.description} />
-              <footer className="card-footer">
-                <ButtonFooter
-                  className="delete-item"
-                  iconClasses="fas fa-trash"
-                  onClick={this.deleteHero}
-                  label="Delete"
-                  dataIndex={index}
-                  dataId={hero.id}
-                />
-                <ButtonFooter
-                  className="edit-item"
-                  iconClasses="fas fa-edit"
-                  onClick={this.selectHero}
-                  label="Edit"
-                  dataIndex={index}
-                  dataId={hero.id}
-                />
-              </footer>
-            </div>
-          </li>
-        ))}
-      </ul>
-    );
+  function selectHero(e) {
+    const hero = getSelectedHero(e);
+    handleSelectHero(hero);
+    history.push(`/heroes/${hero.id}`);
   }
+
+  function deleteHero(e) {
+    const hero = getSelectedHero(e);
+    handleDeleteHero(hero);
+  };
+
+  function getSelectedHero(e) {
+    const index = +e.currentTarget.dataset.index;
+    return heroes[index];
+  }
+
+  // #region render
+  return (
+    <ul className="list">
+      {heroes.map((hero, index) => (
+        <li key={hero.id} role="presentation">
+          <div className="card">
+            <CardContent name={hero.name} description={hero.description} />
+            <footer className="card-footer">
+              <ButtonFooter
+                className="delete-item"
+                iconClasses="fas fa-trash"
+                onClick={deleteHero}
+                label="Delete"
+                dataIndex={index}
+                dataId={hero.id}
+              />
+              <ButtonFooter
+                className="edit-item"
+                iconClasses="fas fa-edit"
+                onClick={selectHero}
+                label="Edit"
+                dataIndex={index}
+                dataId={hero.id}
+              />
+            </footer>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+
+  // #endregion render
 }
 
 export default withRouter(HeroList);
+
+//#region HeroListOld
+
+// import { Component } from 'react'; // move to top of file
+
+// class HeroListOld extends Component {
+//   selectHero = e => {
+//     const index = +e.currentTarget.dataset.index;
+//     const { heroes } = this.props;
+//     const hero = heroes[index];
+
+//     this.props.handleSelectHero(hero);
+//     this.props.history.push(`/heroes/${hero.id}`);
+//   };
+
+//   deleteHero = e => {
+//     const index = +e.currentTarget.dataset.index;
+//     const { heroes } = this.props;
+//     const hero = heroes[index];
+//     this.props.handleDeleteHero(hero);
+//   };
+
+//   render() {
+//     let { heroes /* , selectedHero */ } = this.props;
+
+//     return (
+//       <ul className="list">
+//         {heroes.map((hero, index) => (
+//           <li key={hero.id} role="presentation">
+//             <div className="card">
+//               <CardContent name={hero.name} description={hero.description} />
+//               <footer className="card-footer">
+//                 <ButtonFooter
+//                   className="delete-item"
+//                   iconClasses="fas fa-trash"
+//                   onClick={this.deleteHero}
+//                   label="Delete"
+//                   dataIndex={index}
+//                   dataId={hero.id}
+//                 />
+//                 <ButtonFooter
+//                   className="edit-item"
+//                   iconClasses="fas fa-edit"
+//                   onClick={this.selectHero}
+//                   label="Edit"
+//                   dataIndex={index}
+//                   dataId={hero.id}
+//                 />
+//               </footer>
+//             </div>
+//           </li>
+//         ))}
+//       </ul>
+//     );
+//   }
+// }
+//#endregion HeroListOld
---
+++ src/heroes/useHeroes.js
@@ -0,0 +1,31 @@
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  addHeroAction,
+  deleteHeroAction,
+  loadHeroesAction,
+  selectHeroAction,
+  updateHeroAction
+} from '../store';
+
+/** Custom hook for accessing Hero state in redux store */
+function useHeroes() {
+  const dispatch = useDispatch();
+
+  return {
+    // Selectors
+    heroes: useSelector(state => state.heroes.data),
+    selectedHero: useSelector(state => state.selectedHero),
+
+    // Dispatchers
+    // Wrap any dispatcher that could be called within a useEffect() in a useCallback() 
+    addHero: hero => dispatch(addHeroAction(hero)),
+    deleteHero: hero => dispatch(deleteHeroAction(hero)),
+    getHeroes: useCallback(() => dispatch(loadHeroesAction()), [dispatch]), // called within a useEffect()
+    selectHero: hero => dispatch(selectHeroAction(hero)),
+    updateHero: hero => dispatch(updateHeroAction(hero)), 
+  };
+}
+
+export default useHeroes;
